13217c685ee630a96d378de8725619f1630f7662
wolfgang.lumetsberger@gmail.com <wolfgang.lumetsberger@gmail.com>
2015-10-24 21:12:44 +0200

    create project - add data access layer common - - add base entity and user object - add console application for testing purpose - add data access layer implementation for sql server

---
+++ UlitmateFestivalOrganizer/DAL.Common/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Common.Domain
+{
+    class User : BaseEntity
+    {
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
